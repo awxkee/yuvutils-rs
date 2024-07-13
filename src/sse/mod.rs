d463c3bd7ca87a48047805a779ddd758c0f41bd2
awxkee <radzivon.bartoshyk@proton.me>
2024-07-13 17:57:09 +0100

    Added YUY2 format

--- src/sse/mod.rs
+++ src/sse/mod.rs
@@ -4,12 +4,12 @@
  * // Use of this source code is governed by a BSD-style
  * // license that can be found in the LICENSE file.
  */
-mod sse_support;
-mod sse_ycbcr;
 mod rgb_to_y;
 mod rgb_to_ycgco;
 mod rgb_to_ycgco_r;
 mod rgba_to_yuv;
+mod sse_support;
+mod sse_ycbcr;
 mod sse_ycgco_r;
 mod ycgco_to_rgb;
 mod ycgco_to_rgb_alpha;
@@ -17,9 +17,6 @@ mod ycgcor_to_rgb;
 mod yuv_nv_to_rgba;
 mod yuv_to_rgba;
 mod yuv_to_rgba_alpha;
-mod sse_ycgco_r;
-mod ycgcor_to_rgb;
-mod rgb_to_ycgco_r;
 mod yuv_to_yuy2;
 mod yuy2_to_yuv;
 
@@ -33,7 +30,5 @@ pub use ycgcor_to_rgb::sse_ycgcor_type_to_rgb_row;
 pub use yuv_nv_to_rgba::sse_yuv_nv_to_rgba;
 pub use yuv_to_rgba::sse_yuv_to_rgba_row;
 pub use yuv_to_rgba_alpha::sse_yuv_to_rgba_alpha_row;
-pub use ycgcor_to_rgb::sse_ycgcor_type_to_rgb_row;
-pub use rgb_to_ycgco_r::sse_rgb_to_ycgcor_row;
 pub use yuv_to_yuy2::yuv_to_yuy2_sse_impl;
 pub use yuy2_to_yuv::yuy2_to_yuv_sse_impl;
