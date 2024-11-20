41116160f6e84974a262562322141c6dad061968
Radzivon Bartoshyk <radzivon.bartoshyk@proton.me>
2024-11-20 22:07:22 +0000

    RGBA1010102 WIP

--- src/rgb_ar30.rs
+++ src/rgb_ar30.rs
@@ -61,15 +61,18 @@ fn rgb_to_ar30_impl<
             .chunks_exact(rgba_layout.get_channels_count())
             .zip(dst.iter_mut())
         {
+            let mut r = src[rgba_layout.get_r_channel_offset()] as i32;
+            let mut g = src[rgba_layout.get_g_channel_offset()] as i32;
+            let mut b = src[rgba_layout.get_b_channel_offset()] as i32;
+
+            r = (r << 2) | (r >> 6);
+            g = (g << 2) | (g >> 6);
+            b = (b << 2) | (b >> 6);
+
             let packed = if rgba_layout.has_alpha() {
-                ar30_layout.pack_w_a::<AR30_BYTE_ORDER>(
-                    src[0] as i32,
-                    src[1] as i32,
-                    src[2] as i32,
-                    src[3] as i32 >> 6,
-                )
+                ar30_layout.pack_w_a::<AR30_BYTE_ORDER>(r, g, b, src[3] as i32 >> 6)
             } else {
-                ar30_layout.pack::<AR30_BYTE_ORDER>(src[0] as i32, src[1] as i32, src[2] as i32)
+                ar30_layout.pack::<AR30_BYTE_ORDER>(r, g, b)
             };
             *dst = packed;
         }
