ed7057b3df003bd29627f5a4791363d30a005be6
Kornel <kornel@geekhood.net>
2026-09-15 13:03:17 +0100

    fmt

--- src/shuffle.rs
+++ src/shuffle.rs
@@ -78,10 +78,8 @@ impl ShuffleConverterFactory<u8> for u8 {
         let mut _converter: Box<dyn ShuffleConverter<u8, SRC, DST>> =
             Box::new(Rgba8DefaultConverter::default());
         #[cfg(any(feature = "nightly_avx512", feature = "avx", feature = "sse"))]
-        let (src_channels, dst_channels) = (
-            YuvSourceChannels::from(SRC),
-            YuvSourceChannels::from(DST),
-        );
+        let (src_channels, dst_channels) =
+            (YuvSourceChannels::from(SRC), YuvSourceChannels::from(DST));
 
         #[cfg(feature = "nightly_avx512")]
         if std::arch::is_x86_feature_detected!("avx512bw") {
